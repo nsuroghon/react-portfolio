bd41468f5dc977db521cc05718384d3227e705ca
Niv Suroghon <nsuroghon@gmail.com>
2021-03-07 18:15:48 -0500

    nav route update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav'
+import { Route, BrowserRouter as Router, Link  } from "react-router-dom";
 import About from "./pages/About";
 import '../App.css';
 
 const Navigation = () => {
     return (
-        <Nav className="justify-content-center" activeKey="/home">
+    <Router>
+      <Nav className="justify-content-center" activeKey="/home">
         <Nav.Item>
           <Nav.Link className="link" to="/about">About</Nav.Link>
         </Nav.Item>
@@ -15,7 +17,8 @@ const Navigation = () => {
         <Nav.Item>
           <Nav.Link className="link" eventKey="link-2">Contact</Nav.Link>
         </Nav.Item>
-      </Nav>
+      </Nav>  
+    </Router>
     )
 }
 
