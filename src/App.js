351634e2d985806201fa02636221ba7f0b4678a8
Niv Suroghon <nsuroghon@gmail.com>
2021-03-07 16:37:32 -0500

    contact page first commit & route

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react'
 import './App.css';
 import { Route, BrowserRouter as Router  } from "react-router-dom";
 import Home from './components/pages/Home';
-import About from './components/pages/About'
-import Projects from './components/pages/Projects'
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
 
       </div>
     </Router>
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <>
+        <div>
+            <div className="header">
+                <h1>Contact me</h1>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Contact;
